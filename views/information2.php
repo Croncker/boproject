a9ff0d2185e0fcae6187fcfc7eb670acee3d2393
Lucca <35749@ma-web.nl>
2023-10-31 23:56:49 +0100

    responsive in de homepage

---
+++ views/information2.php
@@ -0,0 +1,22 @@
+<div class="sdg-container2">
+        <h1 class="sdg-title2"><?= $single['title'] ?></h1>
+    <article class="sdg-article2">
+        <section class="sdg-text2">
+            <h2 class="sdg-subtitle2"><?= $single['subtitle'] ?></h2>
+            <p class="sdg-description2"><?= $single['desc'] ?></p>
+        </section>
+        <img src="<?= $single['image'] ?>" alt="<?= $single['title'] ?>" class="sdg-image2-2">
+    </article>
+    <article class="sdg-article2-2">
+        <img src="<?= $single['image2'] ?>" alt="<?= $single['title'] ?>" class="sdg-image2-22">
+        <section class="sdg-text2-2">
+            <h2 class="sdg-subtitle2"><?= $single['subtitle2'] ?></h2>
+            <p class="sdg-description2"><?= $single['desc2'] ?></p>
+            <div class="gametekst">
+            <h1 class="gameh1">Welkom bij SDG Game!</h1>
+            <h2 class="gameh2">De SDG-game is een boeiend online spel dat draait om de 17 duurzame ontwikkelingsdoelen (SDG's). Dit interactieve spel stimuleert studenten om dieper in de SDG's te duiken en uitdagende missies aan te gaan. Hierdoor verwerven ze uitgebreide kennis over elk van de SDG's, geheel in lijn met de Nederlandse normen en waarden voor duurzaamheid.</h2>
+            <a href="http://sdggame.nl/"><button class="grotebutton">Speel nu!</button></a>
+        </div>
+        </section>
+    </article>
+</div>
